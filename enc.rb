0012f43ffc202fb49ae78b5b949b547d751a455f
Luis Lopez <luis.lopez@altoros.com>
2018-08-02 14:57:01 -0300

    Add gemfile, select recipient

--- enc.rb
+++ enc.rb
@@ -2,6 +2,8 @@ require 'gpgme'
 
 crypto = GPGME::Crypto.new
 
+GPGME::Key.import(File.read('Public.asc'))
+
 File.open('secret.gpg', 'w') do |f|
-  crypto.encrypt File.read('secret.txt'), output: f
+  crypto.encrypt File.read('secret.txt'), output: f, recipients: 'nt_amx_asset_detail_G1QB@lumint.com', always_trust: true
 end
