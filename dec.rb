8ef83f234738620a90d7dc0479e5306e22608811
Luis Lopez <luis.lopez@altoros.com>
2018-08-02 18:39:13 -0300

    experiment

--- dec.rb
+++ dec.rb
@@ -1,7 +1,7 @@
 require 'gpgme'
 
 crypto = GPGME::Crypto.new
-cipher = GPGME::Data.new(File.read('secret.gpg'))
-plain = crypto.decrypt cipher
+cipher = GPGME::Data.new(File.read('secret.txt.gpg'))
+plain = crypto.decrypt cipher, password: "NewP4ssC0", passphrase_callback_value: "NewP4ssC0"
 
 puts(plain.read)
--- enc.rb
+++ enc.rb
@@ -4,6 +4,6 @@ crypto = GPGME::Crypto.new
 
 GPGME::Key.import(File.read('Public.asc'))
 
-File.open('secret.gpg', 'w') do |f|
+File.open('secret.txt.gpg', 'w') do |f|
   crypto.encrypt File.read('secret.txt'), output: f, recipients: 'nt_amx_asset_detail_G1QB@lumint.com', always_trust: true
 end
